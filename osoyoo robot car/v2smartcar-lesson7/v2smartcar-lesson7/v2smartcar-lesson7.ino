2c0921942a3798232577a5da834b4c5e6561a4de
Awais Amjad <101062851+awaisamjad@users.noreply.github.com>
2023-12-21 01:46:08 +0500

    Update v2smartcar-lesson7.ino

--- osoyoo robot car/v2smartcar-lesson7/v2smartcar-lesson7/v2smartcar-lesson7.ino
+++ osoyoo robot car/v2smartcar-lesson7/v2smartcar-lesson7/v2smartcar-lesson7.ino
@@ -1,233 +1,197 @@
-/*  ___   ___  ___  _   _  ___   ___   ____ ___  ____  
- * / _ \ /___)/ _ \| | | |/ _ \ / _ \ / ___) _ \|    \ 
- *| |_| |___ | |_| | |_| | |_| | |_| ( (__| |_| | | | |
- * \___/(___/ \___/ \__  |\___/ \___(_)____)___/|_|_|_|
- *                  (____/ 
- * Arduino Smart Car Tutorial Lesson 7
- * Tutorial URL http://osoyoo.com/2019/02/26/osoyoo-v2-robot-car-lesson-7-imitation-driving-with-bluetooth-app/
- * CopyRight www.osoyoo.com
+#define speedPinL 6    // ENB
+#define leftWheelPin1  7    //Left Motor direction pin 1 to MODEL-X IN3 
+#define leftWheelPin2  8   //Left Motor direction pin 2 to MODEL-X IN4
+#define speedPinR 5    //  ENA
+#define rightWheelPin1  12    //Right Motor direction pin 1 to MODEL-X IN1 
+#define rightWheelPin2 11    //Right Motor direction pin 2 to MODEL-X IN2
 
- * After running the code, smart car will go forward 5 seconds, then go backward 5
- * seconds, then left turn for 5 seconds then right turn for 5 seconds then stop. 
- * 
- */
- 
-#include <SoftwareSerial.h>
-SoftwareSerial BLTSerial(4, 5);  //RX,TX
- 
-int buttonState;
- 
-#define dir1PinRight   12  // Right Motor direction pin 1 to MODEL-X IN1 
-#define dir2PinRight  11   // Right Motor direction pin 2 to MODEL-X IN2
-#define speedPinRight 9   //RIGHT PWM pin connect MODEL-X ENA
-#define speedPinLeft 6    // Left PWM pin connect MODEL-X ENB
-#define dir1PinLeft  7    //Left Motor direction pin 1 to MODEL-X IN3 
-#define dir2PinLeft  8    //Left Motor direction pin 2 to MODEL-X IN4 
-   //back speed
-#define MAX_PACKETSIZE 32    //Serial receive buffer
+#define LFSensor_0 A0 // We define LFSensor_0 to hold the left most value of the IR sensor. This IR sensor is connected to pin A0 as an analog pin is required to read continous IR values. This is then repeated for each subsequent IR sensor
 
-struct car_status{
-  int speed;
-  int angle;
-  int direct;
-};
-char buffUART[MAX_PACKETSIZE];
-unsigned int buffUARTIndex = 0;
-unsigned long preUARTTick = 0;
-int SPEED =150;  //avoidance motor speed 
-int TURNSPEED=50  ;
-/*motor control*/
-void go_Advance(void)  //Forward
-{
-  digitalWrite(dir1PinRight, HIGH);
-  digitalWrite(dir2PinRight,LOW);
-  digitalWrite(dir1PinLeft,HIGH);
-  digitalWrite(dir2PinLeft,LOW);
+#define LFSensor_1 A1 // Holds second IR sensor value
+#define LFSensor_2 A2 // Holds third IR sensor value
+#define LFSensor_3 A3 // Holds fourth IR sensor value
+#define LFSensor_4 A4 // Holds fifth IR sensor value
 
-}
-void go_Left()  //Turn left
-{
-  digitalWrite(dir1PinRight, HIGH);
-  digitalWrite(dir2PinRight,LOW);
-  digitalWrite(dir1PinLeft,LOW);
-  digitalWrite(dir2PinLeft,HIGH);
-}
-void go_Right()  //Turn right
-{
-  digitalWrite(dir1PinRight, LOW);
-  digitalWrite(dir2PinRight,HIGH);
-  digitalWrite(dir1PinLeft,HIGH);
-  digitalWrite(dir2PinLeft,LOW);
-}
-void go_Back()  //Reverse
-{
-  digitalWrite(dir1PinRight, LOW);
-  digitalWrite(dir2PinRight,HIGH);
-  digitalWrite(dir1PinLeft,LOW);
-  digitalWrite(dir2PinLeft,HIGH);
-}
-void back_Left()  //Reverse
-{
-  digitalWrite(dir1PinRight, LOW);
-  digitalWrite(dir2PinRight,HIGH);
-  digitalWrite(dir1PinLeft,HIGH);
-  digitalWrite(dir2PinLeft,LOW);
-}
-void back_Right()  //Reverse
-{
-  digitalWrite(dir1PinRight, HIGH);
-  digitalWrite(dir2PinRight,LOW);
-  digitalWrite(dir1PinLeft,LOW);
-  digitalWrite(dir2PinLeft,HIGH);
-}
-void stop_Stop(){
-    digitalWrite(dir1PinRight,LOW);
-  digitalWrite(dir2PinRight,LOW);
-  digitalWrite(dir1PinLeft,LOW);
-  digitalWrite(dir2PinLeft,LOW);
-   set_Motorspeed(0,0) ;
-}
 
-/*set motor speed */
+#define FAST_SPEED 150
+#define MID_SPEED 140
+#define SLOW_SPEED 130   //back speed
+
+#define trigPin 2
+#define echoPin 3
+
+#include <Servo.h>
+Servo myservo;
+
 void set_Motorspeed(int speed_L,int speed_R)
 {
-  analogWrite(speedPinRight,speed_R); 
-  analogWrite(speedPinLeft,speed_L);   
+  analogWrite(speedPinL,speed_L); 
+  analogWrite(speedPinR,speed_R);   
+}
+
+void go_Forward(){
+  digitalWrite(rightWheelPin1,HIGH);
+  digitalWrite(rightWheelPin2,LOW);
+  digitalWrite(leftWheelPin1,HIGH);
+  digitalWrite(leftWheelPin2,LOW);
+  set_Motorspeed(200,200);
+  
+}
+
+void go_Back(){
+  digitalWrite(rightWheelPin1,LOW);
+  digitalWrite(rightWheelPin2,HIGH);
+  digitalWrite(leftWheelPin1,LOW);
+  digitalWrite(leftWheelPin2,HIGH);
+  set_Motorspeed(200,200);
+
+}
+
+void go_Left(){
+  digitalWrite(rightWheelPin1,HIGH);
+  digitalWrite(rightWheelPin2,LOW);
+  digitalWrite(leftWheelPin1,LOW);
+  digitalWrite(leftWheelPin2,HIGH);
+  set_Motorspeed(200,200);
+}
+
+void go_Right(){
+  digitalWrite(rightWheelPin1,LOW);
+  digitalWrite(rightWheelPin2,HIGH);
+  digitalWrite(leftWheelPin1,HIGH);
+  digitalWrite(leftWheelPin2,LOW);
+  set_Motorspeed(200,200);
+}
+
+void stop_Robot(){
+  digitalWrite(rightWheelPin1,LOW);
+  digitalWrite(rightWheelPin2,LOW);
+  digitalWrite(leftWheelPin1,LOW);
+  digitalWrite(leftWheelPin2,LOW);
+}
+
+// — — — — — — Explanation of how the TSM values are stored and handled — — — — — — 
+// To represent our line detection input, we use binary. If the Tracking Sensor Module (TSM) detects black, it registers as 1; otherwise, it's 0. 
+// For instance, if the TSM detects black on the leftmost sensor, it will read '10000', with '1' representing black and '0' representing white. 
+// We work with binary numbers to represent the sensor's view of black and white, so our calculations start in decimal and later convert to binary.
+
+// A function that reads sensor values and returns them as type 'String'.
+String read_sensor_values() {
+    int sensorvalue = 32; // Initialize a variable called sensorvalue and set it equal to 32 (100000 in binary). We use 6 digits for accurate binary addition; the 6th digit, the '1', will be dropped, leaving a 5-digit sensor reading.
+
+  // Read the values of the five sensors and invert them using the '!' (NOT) operator. Assign these inverted values to the corresponding positions in the sensor array.
+    sensor[0] = !digitalRead(LFSensor_0);
+    sensor[1] = !digitalRead(LFSensor_1);
+    sensor[2] = !digitalRead(LFSensor_2);
+    sensor[3] = !digitalRead(LFSensor_3);
+    sensor[4] = !digitalRead(LFSensor_4);
+
+// ^^^
+// According to the information on [3], black is active at LOW (0), and white is active at HIGH (1). We want the opposite, so we use the exclamation mark when reading the value. 
+// We use digitalRead instead of analogRead as we need an integer value of either 1 or 0. 
+// DigitalRead provides either HIGH (1) or LOW (0) states, while analogRead can have any value between 0 and 1023.
+
+// The binary values we obtain must be converted to decimal by multiplying each bit by 2 to the power of its index.
+// Even though are values of type 'char' we can perform mathematical operations on them as C++ will convert them into 'int' types
+// sensor[0] is a char type that can be either 1 or 0. when multiplied by 16 it becomes either 16 or 0 which is added to sensor value. This is then repeated for all other values
+    sensorvalue += (sensor[0] * 16) + (sensor[1] * 8) + (sensor[2] * 4) + (sensor[3] * 2) +      (sensor[4]);
+
+// Powers of 2 are used as these numbers are requied to convert to binary
+//Then, convert the sensorvalue to binary and remove the last digit using substring. Finally, return the result.
+    String senstr = String(sensorvalue, BIN);
+    senstr = senstr.substring(1, 6);
+    return senstr;
 }
 
 
-//WiFi / Bluetooth through the serial control
-void do_Uart_Tick()
-{
-
-  char Uart_Date=0;
-  if(BLTSerial.available()) 
+void lineFollowing(){
+ String sensorval= read_sensor_values();
+  //Serial.println(sensorval);
+  if (   sensorval=="10000" )
+  { 
+    //The black line is in the left of the car, need  left turn 
+    go_Left();  //Turn left
+    set_Motorspeed(FAST_SPEED,FAST_SPEED);
+  }
+  if (sensorval=="10100"  || sensorval=="01000" || sensorval=="01100" || sensorval=="11100"  || sensorval=="10010" || sensorval=="11010")
   {
-    size_t len = BLTSerial.available();
-    uint8_t sbuf[len + 1];
-    sbuf[len] = 0x00;
-    BLTSerial.readBytes(sbuf, len);
-    //parseUartPackage((char*)sbuf);
-    memcpy(buffUART + buffUARTIndex, sbuf, len);//ensure that the serial port can read the entire frame of data
-    buffUARTIndex += len;
-    preUARTTick = millis();
-    if(buffUARTIndex >= MAX_PACKETSIZE - 1) 
-    {
-      buffUARTIndex = MAX_PACKETSIZE - 2;
-      preUARTTick = preUARTTick - 200;
-    }
+    go_Forward();  //Turn slight left
+    set_Motorspeed(0,FAST_SPEED);
   }
-  car_status cs;
-  if(buffUARTIndex > 0 && (millis() - preUARTTick >= 100))//APP send flag to modify the obstacle avoidance parameters
-  { //data ready
-    buffUART[buffUARTIndex] = 0x00;
-    Uart_Date=buffUART[0];
-    cs=get_status(buffUART);
-    buffUARTIndex = 0;
+  if (    sensorval=="00001"  ){ //The black line is  on the right of the car, need  right turn 
+    go_Right();  //Turn right
+    set_Motorspeed(FAST_SPEED,FAST_SPEED);
   }
-   SPEED=cs.speed+50;
-   TURNSPEED=SPEED*3/2;
-   if (SPEED>250) SPEED=250;
-   if (TURNSPEED>250) TURNSPEED=250;
-  switch (Uart_Date)    //serial control instructions
+  if (sensorval=="00011" || sensorval=="00010"  || sensorval=="00101" || sensorval=="00110" || sensorval=="00111" || sensorval=="01101" || sensorval=="01111"   || sensorval=="01011"  || sensorval=="01001")
   {
-    case 'M':  
-    go_Advance();
-    set_Motorspeed(SPEED,SPEED) ;
-    break;
-    case 'L':  
-    go_Left(); 
-    if(cs.angle=1){
-       set_Motorspeed(0,50) ;
-    }
-    if(cs.angle=2){
-       set_Motorspeed(50,200) ;
-    }
-    break;
-    case 'R': 
-    go_Right(); 
-    if(cs.angle=-1){
-       set_Motorspeed(50,0) ;
-    }
-    if(cs.angle=-2){
-       set_Motorspeed(200,50) ;
-    }
-    break;
-    case 'B':  
-    go_Back(); 
-       set_Motorspeed(SPEED,SPEED) ;
-    break;
-    case 'X': 
-    back_Left();
-    if(cs.angle=1){
-       set_Motorspeed(0,50) ;
-    }
-    if(cs.angle=2){
-       set_Motorspeed(50,200) ;
-    }
-    break;
-    case 'Y':  
-    back_Right(); 
-    if(cs.angle=-1){
-       set_Motorspeed(50,0) ;
-    }
-    if(cs.angle=-2){
-       set_Motorspeed(200,50) ;
-    }
-    break;
- 
-    case 'E': stop_Stop() ;break;
-    case 'J': stop_Stop() ;break;
-    default:break;
+    go_Forward();  //Turn slight right
+    set_Motorspeed( FAST_SPEED,0);
   }
-}
-
-//car motor control
-void setup()
-{
  
-  pinMode(dir1PinRight, OUTPUT); 
-  pinMode(dir2PinRight, OUTPUT); 
-  pinMode(speedPinRight, OUTPUT);  
-  pinMode(dir1PinLeft, OUTPUT);
-  pinMode(dir2PinLeft, OUTPUT); 
-  pinMode(speedPinLeft, OUTPUT); 
-  stop_Stop();
-   
-  Serial.begin(9600);//In order to fit the Bluetooth module's default baud rate, only 9600
-   BLTSerial.begin(9600);
- 
-}
-int lastButtonState=0;
-void loop()
-{  
-  do_Uart_Tick();
- 
-}
-
-car_status get_status( char buffUART[])
-{
-  car_status cstatus;
-  int index=2;
-  if (buffUART[index]=='-'){
-    cstatus.angle=-buffUART[index+1]+'0';
-    index=index+3;
+  if (sensorval=="11111"){
+    stop_Robot();   //The car front touch stop line, need stop
+  }
     
-  } else {
-   
-    cstatus.angle=buffUART[index]-'0';
-     index=index+2;
-  }
-  int currentvalue;
-  int spd=0;
-  while (buffUART[index]!=',')
-  {
-    currentvalue=buffUART[index]-'0';
-    spd=spd*10+currentvalue;
-    index++;
-  }
-  cstatus.speed=spd;
-  index++;
-  cstatus.direct=buffUART[index]-'0';
-  return cstatus;
+}
+float sendPing(){
+  float distance;
+  long duration;
+  digitalWrite(trigPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin,LOW);
+  duration = pulseIn(echoPin,HIGH);
+  distance = duration*0.034/2;
+  return (distance);
+}
+
+void obstacleAvoidance(){
+  go_Right();
+  delay(500);
+  go_Forward();
+  delay(460);
+  go_Left();
+  delay(500);
+  go_Forward();
+  delay(600);
+  go_Left();
+  delay(400);
+  go_Forward();
+  delay(300);
+
+}
+
+
+void setup() {
+
+  myservo.attach(13);
+  myservo.write(90);
+  delay(500);
+
+  pinMode(speedPinL,OUTPUT);
+  pinMode(leftWheelPin1,OUTPUT);
+  pinMode(leftWheelPin2,OUTPUT);
+  pinMode(speedPinR,OUTPUT);
+  pinMode(rightWheelPin1,OUTPUT);
+  pinMode(rightWheelPin2,OUTPUT);
+
+  stop_Robot(); 
+
+  digitalWrite(speedPinL,HIGH);
+  digitalWrite(speedPinR,HIGH);
+
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+
+  
+}
+
+void loop() {
+
+    lineFollowing();
+    if(sendPing()<8){
+      obstacleAvoidance();
+    }
+
 }
